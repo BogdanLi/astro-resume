c96d84e4e49f218f0e969cd8a8a703b15686fa0f
BogdanLi <li.bogdan44@gmail.com>
2024-08-21 16:48:41 +0500

    first loc

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+export const languages = {
+  en: "English",
+  ru: "Русский",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "bogdan.name": "Bogdan Li",
+    "bogdan.position": "React.js Developer",
+  },
+  ru: { "bogdan.name": "Богдан Ли", "bogdan.position": "React.js Разработчик" },
+} as const;
