c9681d849ef2a30ec5b3eae5d871d11a271330e5
BogdanLi <li.bogdan44@gmail.com>
2024-08-28 14:59:19 +0500

    finished localization

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,6 +9,103 @@ export const ui = {
   en: {
     "bogdan.name": "Bogdan Li",
     "bogdan.position": "React.js Developer",
+    "profile.title": "Profile",
+    "profile.value":
+      "Front-End Developer with extensive experience in modern JavaScript frameworks and libraries, specializing in React.js. Proficient in TypeScript, Tailwind CSS, and Material UI. Demonstrates versatility across diverse project environments and a strong commitment to professional growth. Proven track record in developing sophisticated landing pages and robust administrative interfaces. Adept at quickly adapting to new technologies and methodologies to drive project success.",
+    "contacts.title": "Contacts",
+    "skills.title": "Skills",
+    "education.title": "Education",
+    "education.degree": "High School Degree",
+    "education.subtitle":
+      "Federal State Government Institution Secondary School №6 ",
+    "languages.title": "Languages",
+    "languages.ru": "Russian",
+    "languages.en": "English",
+    "languages.ko": "Korean",
+    "workBackground.title": "work experience",
+    "workBackground.position1": "Course Intructor",
+    "workBackground.position2": "Course Intructor",
+    "workBackground.position3": "Front-end Developer",
+    "workBackground.company1": "skills accelerator Ilmhona",
+    "workBackground.company2": "Megafon Tajikistan",
+    "workBackground.company3": "Livo",
+    "workBackground.date1": "MAY 2023 - PRESENT",
+    "workBackground.date2": "NOVEMBER 2023 - MARCH 2024",
+    "workBackground.date3": "JANUARY 2024 - PRESENT",
+    "workBackground.key1":
+      "Revamped and modernized three foundational tech courses: Introduction to JavaScript, Introduction to Python, and Front-End Development from Scratch, resulting in a 50% increase in student comprehension and engagement.",
+    "workBackground.key2":
+      "Pioneered two innovative courses: Website Development with Tilda and React.js Bootcamp, specifically designed to enhance employability and secure internships for young professionals in the tech industry.",
+    "workBackground.key3":
+      "Dramatically improved course effectiveness through strategic restructuring and content updates.",
+    "workBackground.key4":
+      "Facilitated direct pathways to employment and practical experience for students through industry-aligned curriculum design.",
+    "workBackground.key5":
+      "Enhanced accessibility and personalization of learning materials through the development of a bespoke digital platform.",
+    "workBackground.key6":
+      "Delivered comprehensive Python training programs to multiple employee groups, focusing on advanced data analysis techniques.",
+    "workBackground.key7":
+      "Designed and implemented a curriculum that emphasized practical applications of Python and introductory Pandas for corporate data analysis.",
+    "workBackground.key8":
+      "Facilitated cross-departmental data literacy, significantly enhancing the company's overall data analysis capabilities.",
+    "workBackground.key9":
+      "Spearheaded the development of cutting-edge user interfaces, leveraging the latest front-end technologies to enhance user experience and visual appeal.",
+    "workBackground.key10":
+      "Consistently delivered projects ahead of schedule by optimizing development processes and providing crucial support to team members.",
+    "workBackground.key11":
+      "Implemented effective strategies to guide team members in completing tasks efficiently, ensuring timely project completion.",
+    "workBackground.key12":
+      "Architected and developed a state-of-the-art website for the company, incorporating modern technologies to improve performance, scalability, and user engagement.",
   },
-  ru: { "bogdan.name": "Богдан Ли", "bogdan.position": "React.js Разработчик" },
+  ru: {
+    "bogdan.name": "Богдан Ли",
+    "bogdan.position": "React.js Разработчик",
+    "profile.title": "Профиль",
+    "profile.value":
+      "Фронтенд-разработчик с обширным опытом работы с современными JavaScript-фреймворками и библиотеками, специализирующийся на React.js. Владеет TypeScript, Tailwind CSS и Material UI. Демонстрирует универсальность в различных проектных средах и сильную приверженность профессиональному росту. Имеет подтвержденный опыт в разработке сложных целевых страниц и надежных административных интерфейсов. Умеет быстро адаптироваться к новым технологиям и методологиям для обеспечения успеха проекта.",
+    "contacts.title": "Контакты",
+    "skills.title": "Навыки",
+    "education.title": "Образование",
+    "education.degree": "Среднее Общее",
+    "education.subtitle":
+      "Федеральное Казенное Государственное Общеобразовательное Учреждение СОШ №6",
+    "languages.title": "Языки",
+    "languages.ru": "Русский",
+    "languages.en": "Английский",
+    "languages.ko": "Корейский",
+    "workBackground.title": "опыт работы",
+    "workBackground.position1": "Инструктор курсов",
+    "workBackground.position2": "Инструктор курсов",
+    "workBackground.position3": "Front-end Разработчик",
+    "workBackground.company1": "акселератор навыков Илмхона",
+    "workBackground.company2": "Мегафон Таджикистан",
+    "workBackground.company3": "Ливо",
+    "workBackground.date1": "МАЙ 2023 - Н.В.",
+    "workBackground.date2": "НОЯБРЬ 2023 - МАРТ 2024",
+    "workBackground.date3": "ЯНВАРЬ 2024 - Н.В.",
+    "workBackground.key1":
+      "Обновил и модернизировал три базовых технических курса: Введение в JavaScript, Введение в Python и Фронтенд-разработка с нуля, что привело к 50% увеличению понимания и вовлеченности студентов.",
+    "workBackground.key2":
+      "Разработал два инновационных курса: Разработка сайтов на Tilda и Интенсив по React.js, специально предназначенные для повышения возможностей трудоустройства и получения стажировок для молодых специалистов в технологической индустрии.",
+    "workBackground.key3":
+      "Значительно повысил эффективность курсов путем стратегической реструктуризации и обновления контента.",
+    "workBackground.key4":
+      "Обеспечил прямые пути к трудоустройству и практическому опыту для студентов через разработку учебных программ, согласованных с требованиями индустрии.",
+    "workBackground.key5":
+      "Улучшил доступность и персонализацию учебных материалов путем разработки специализированной цифровой платформы.",
+    "workBackground.key6":
+      "Провел комплексные программы обучения Python для нескольких групп сотрудников, уделяя особое внимание продвинутым методам анализа данных. ",
+    "workBackground.key7":
+      "Разработал и внедрил учебную программу, которая подчеркивала практическое применение Python и вводный курс по Pandas для корпоративного анализа данных.",
+    "workBackground.key8":
+      "Способствовал повышению грамотности в области данных между отделами, значительно улучшив общие возможности компании по анализу данных.",
+    "workBackground.key9":
+      "Возглавил разработку передовых пользовательских интерфейсов, используя новейшие фронтенд-технологии для улучшения пользовательского опыта и визуальной привлекательности.",
+    "workBackground.key10":
+      "Стабильно завершал проекты раньше срока за счет оптимизации процессов разработки и оказания важной поддержки членам команды.",
+    "workBackground.key11":
+      "Внедрил эффективные стратегии для руководства членами команды в эффективном выполнении задач, обеспечивая своевременное завершение проектов.",
+    "workBackground.key12":
+      "Спроектировал и разработал современный веб-сайт для компании, внедряя современные технологии для улучшения производительности, масштабируемости и вовлеченности пользователей.",
+  },
 } as const;
